b2ffa8b602b738a9de45c1cf679d08fdaa00697c
Fabienholert <fabienholert@gmail.com>
2025-03-10 23:01:28 +0100

    Ajout des pages Projet1 et Projet2, mise à jour du composant Home et amélioration des styles

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./layout.jsx";
 import Home from "./pages/Home/Home.jsx";
+import Projet1 from "./pages/projet1/projet1.jsx";
+import Projet2 from "./pages/projet2/projet2.jsx";
 
 export default function App() {
   return (
@@ -9,6 +11,8 @@ export default function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
+            <Route path="/Projet1" element={<Projet1 />} />
+            <Route path="/Projet2" element={<Projet2 />} />
           </Route>
         </Routes>
       </BrowserRouter>
--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import "./Home.scss";
 
-export function Home() {
+export default function Home() {
   return (
     <>
-      <div className="home"></div>
-      <h2>Fabien Holert</h2>
+      <div className="home__container"></div>
+      <h2 className="home__nom">Fabien Holert</h2>
     </>
   );
 }
