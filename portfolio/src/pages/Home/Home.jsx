032889b9cf82a3d26f3fd2ff4d14524b5aa04c0e
Fabienholert <fabienholert@gmail.com>
2025-03-10 20:02:32 +0100

    Ajout du composant Home avec un titre et importation des styles SCSS

--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,7 +1,11 @@
-export default function Home() {
+import React from "react";
+import "./Home.scss";
+
+export function Home() {
   return (
     <>
       <div className="home"></div>
+      <h2>Fabien Holert</h2>
     </>
   );
 }
