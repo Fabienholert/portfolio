150d5ae50608ce930cfd542a76f01d8cd6a0ffda
Fabienholert <fabienholert@gmail.com>
2025-03-11 22:38:21 +0100

    Mise à jour du composant Section : réorganisation des informations sur les langages, frameworks et outils utilisés, avec une meilleure structuration des titres.

--- portfolio/src/components/section/section.jsx
+++ portfolio/src/components/section/section.jsx
@@ -1,12 +1,23 @@
 export default function Section() {
   return (
     <div>
-      <h3 className="Section__h3">Framework utilisé</h3>
-      <div className="Section__ul">
-        <li>React </li>
-        <li>Javascript</li>
-        <li>Nodejs</li>
-      </div>
+      <h3 className="Section__frontEnd">
+        Front-end
+        <p>Langages : Javascript </p>
+        <p>Frameworks et Bibliothèques: React </p>
+        <p>Intégration Web: HTML , CSS , SASS</p>
+      </h3>
+      <h3 className="Section__Backend">
+        Back-end
+        <p>Langages et Frameworks: Nodejs, Express.js</p>
+        <p>Base de Données: MongoDB</p>
+        <p>API: Création et consommation d'API REST</p>
+      </h3>
+      <h3 className="Outils">
+        Outils
+        <p>Gestion de version: Git, Github</p>
+        <p>Environnements: Visual Studio Code , Github</p>
+      </h3>
     </div>
   );
 }
