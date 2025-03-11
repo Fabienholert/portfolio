04710e443ba7045dc5ca7183a652e6ec87cfc376
Fabienholert <fabienholert@gmail.com>
2025-03-11 22:25:54 +0100

    Ajout du composant Section pour afficher les frameworks utilisés et mise à jour du composant Home pour l'intégrer.

---
+++ portfolio/src/components/section/section.jsx
@@ -0,0 +1,12 @@
+export default function Section() {
+  return (
+    <div>
+      <h3 className="Section__h3">Framework utilisé</h3>
+      <div className="Section__ul">
+        <li>React </li>
+        <li>Javascript</li>
+        <li>Nodejs</li>
+      </div>
+    </div>
+  );
+}
--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Section from "../../components/section/section";
 import "./Home.scss";
 
 export default function Home() {
   return (
-    <div className="home__Container">
-      <h2 className="home__nom">Fabien Holert</h2>
+    <div>
+      <div className="home__Container">
+        <h2 className="home__nom">Fabien Holert</h2>
+      </div>
+      <Section />
     </div>
   );
 }
