c8cc4a21d850dd877781465e563dd0fe7c236072
Fabienholert <fabienholert@gmail.com>
2025-03-17 23:21:41 +0100

    Ajout de la dépendance "react-responsive-carousel" et mise à jour de la page d'accueil avec une photo, un titre, un sous-titre et une description.

--- portfolio/src/pages/Home/Home.jsx
+++ portfolio/src/pages/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
+import Photo from "../../assets/moi.webp";
 import Card from "../../components/Card/Card";
-import Section from "../../components/section/section";
 import "./Home.scss";
 
 export default function Home() {
@@ -8,8 +8,15 @@ export default function Home() {
     <div>
       <div className="home__Container">
         <h2 className="home__nom">Fabien Holert</h2>
+        <h3 className="home__poste">Développeur Web</h3>
+        <img Moi src={Photo} alt="moi" className="home__photo" />
+        <p className="home__description">
+          Passionné par le développement web, je suis actuellement en formation
+          chez OpenClassrooms pour devenir développeur web. J'ai acquis des
+          compétences en HTML, CSS, Javascript, React, Nodejs, Express.js et
+          MongoDB.
+        </p>
       </div>
-      <Section />
       <Card />
     </div>
   );
