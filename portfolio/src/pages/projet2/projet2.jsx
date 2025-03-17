406f9eaf947daee85f7315027eee3e70045da7d2
Fabienholert <fabienholert@gmail.com>
2025-03-17 18:45:52 +0100

    Mise à jour du composant Projet2 : réorganisation du contenu avec des balises <h3> et <p> pour une meilleure lisibilité et structure.

--- portfolio/src/pages/projet2/projet2.jsx
+++ portfolio/src/pages/projet2/projet2.jsx
@@ -1,55 +1,98 @@
 export default function Projet2() {
   return (
     <p>
-      Réalisations : Conception de l'API : Sur la base des spécifications
-      techniques fournies, j'ai conçu une API RESTful complète pour gérer les
-      livres, les utilisateurs, les notes et les commentaires. Développement du
-      back-end : J'ai mis en œuvre l'API en utilisant [Choisissez ici votre
-      technologie back-end préférée : Node.js avec Express, Python avec
-      Flask/Django, etc.]. Cela comprenait la création de routes, la gestion des
-      requêtes HTTP, la validation des données et l'interaction avec une base de
-      données [Choisissez ici votre base de données : MongoDB, PostgreSQL,
-      MySQL, etc.]. Gestion des utilisateurs : J'ai intégré un système
-      d'authentification et d'autorisation pour gérer les utilisateurs, leurs
-      rôles et leurs permissions. Optimisation des images : J'ai implémenté une
-      fonctionnalité d'optimisation des images côté serveur pour réduire leur
-      taille et améliorer les performances du site, en tenant compte des
-      principes du Green Code. Tests unitaires et d'intégration : J'ai écrit des
-      tests unitaires et d'intégration pour garantir la qualité et la fiabilité
-      de l'API. Déploiement : J'ai déployé l'API sur un serveur [Choisissez
-      votre plateforme de déploiement : Heroku, AWS, Google Cloud, etc.] et
-      configuré les paramètres nécessaires pour une performance optimale.
-      Problématiques rencontrées et solutions : Compréhension des besoins du
-      front-end : Bien que le front-end ait été développé en parallèle, il était
-      essentiel de comprendre ses besoins spécifiques pour concevoir une API
-      adaptée. J'ai maintenu une communication régulière avec le développeur
-      front-end pour clarifier les exigences et résoudre les problèmes
-      éventuels. Optimisation des images : L'optimisation des images était un
-      défi technique, car il fallait trouver un équilibre entre la réduction de
-      la taille des fichiers et le maintien d'une qualité visuelle acceptable.
-      J'ai exploré différentes bibliothèques d'optimisation d'images et choisi
-      celle qui répondait le mieux aux besoins du projet. Scalabilité : Il était
-      important de concevoir l'API de manière à ce qu'elle puisse gérer un
-      nombre croissant d'utilisateurs et de données. J'ai utilisé des techniques
-      telles que la mise en cache et l'optimisation des requêtes de base de
-      données pour améliorer la scalabilité. La conformité au Green Code : La
-      réduction de la taille des images a permis de rendre le site moins
-      "gourmand" en ressource et donc moins impactant. Compétences développées :
-      Développement d'API RESTful : J'ai acquis une solide expérience dans la
-      conception et le développement d'API RESTful en utilisant [Votre
-      technologie back-end]. Gestion de base de données : J'ai amélioré mes
-      compétences dans la gestion de bases de données [Votre base de données],
-      notamment dans la conception de schémas, l'optimisation des requêtes et la
-      gestion des transactions. Authentification et autorisation : J'ai appris à
-      mettre en œuvre des systèmes d'authentification et d'autorisation
-      sécurisés pour protéger les données des utilisateurs. Optimisation des
-      performances : J'ai développé des compétences en optimisation des
-      performances, notamment dans la mise en cache, l'optimisation des requêtes
-      et la réduction de la taille des fichiers. Collaboration : J'ai renforcé
-      mes compétences en collaboration avec d'autres développeurs, notamment
-      dans la communication, la résolution de problèmes et le partage de
-      connaissances. Green Code : Intégration des pratiques du Green Code dans
-      un projet, et les bénéfices que cela peut apporter.
+      <h3>Réalisations :</h3>
+      <p>
+        Conception de l'API : Sur la base des spécifications techniques
+        fournies, j'ai conçu une API RESTful complète pour gérer les livres, les
+        utilisateurs, les notes et les commentaires.
+      </p>
+      <p>
+        {" "}
+        Développement du back-end : J'ai mis en œuvre l'API en utilisant Node.js
+        avec Express. Cela comprenait la création de routes, la gestion des
+        requêtes HTTP, la validation des données et l'interaction avec une base
+        de données MongoDB.{" "}
+      </p>
+      <p>
+        Gestion des utilisateurs : J'ai intégré un système d'authentification et
+        d'autorisation pour gérer les utilisateurs, leurs rôles et leurs
+        permissions.
+      </p>
+      <p>
+        Optimisation des images : J'ai implémenté une fonctionnalité
+        d'optimisation des images côté serveur pour réduire leur taille et
+        améliorer les performances du site, en tenant compte des principes du
+        Green Code.
+      </p>
+      <p>
+        {" "}
+        Tests unitaires et d'intégration : J'ai écrit des tests unitaires et
+        d'intégration pour garantir la qualité et la fiabilité de l'API.
+      </p>
+      <p>
+        Déploiement : J'ai déployé l'API sur un serveur [Choisissez votre
+        plateforme de déploiement : Heroku, AWS, Google Cloud, etc.] et
+        configuré les paramètres nécessaires pour une performance optimale.
+      </p>
+      <h3>Problématiques rencontrées et solutions :</h3>
+      <p>
+        Compréhension des besoins du front-end : Bien que le front-end ait été
+        développé en parallèle, il était essentiel de comprendre ses besoins
+        spécifiques pour concevoir une API adaptée. J'ai maintenu une
+        communication régulière avec le développeur front-end pour clarifier les
+        exigences et résoudre les problèmes éventuels.
+      </p>
+      <p>
+        Optimisation des images : L'optimisation des images était un défi
+        technique, car il fallait trouver un équilibre entre la réduction de la
+        taille des fichiers et le maintien d'une qualité visuelle acceptable.
+        J'ai exploré différentes bibliothèques d'optimisation d'images et choisi
+        celle qui répondait le mieux aux besoins du projet.
+      </p>
+      <p>
+        Scalabilité : Il était important de concevoir l'API de manière à ce
+        qu'elle puisse gérer un nombre croissant d'utilisateurs et de données.
+        J'ai utilisé des techniques telles que la mise en cache et
+        l'optimisation des requêtes de base de données pour améliorer la
+        scalabilité.{" "}
+      </p>
+      <p>
+        La conformité au Green Code : La réduction de la taille des images a
+        permis de rendre le site moins "gourmand" en ressource et donc moins
+        impactant.
+      </p>
+      <h3>Compétences développées :</h3>
+      <p>
+        Développement d'API RESTful : J'ai acquis une solide expérience dans la
+        conception et le développement d'API RESTful en utilisant [Votre
+        technologie back-end].{" "}
+      </p>
+      <p>
+        Gestion de base de données : J'ai amélioré mes compétences dans la
+        gestion de bases de données [Votre base de données], notamment dans la
+        conception de schémas, l'optimisation des requêtes et la gestion des
+        transactions.
+      </p>
+      <p>
+        Authentification et autorisation : J'ai appris à mettre en œuvre des
+        systèmes d'authentification et d'autorisation sécurisés pour protéger
+        les données des utilisateurs.
+      </p>
+      <p>
+        Optimisation des performances : J'ai développé des compétences en
+        optimisation des performances, notamment dans la mise en cache,
+        l'optimisation des requêtes et la réduction de la taille des fichiers.{" "}
+      </p>
+      <p>
+        Collaboration : J'ai renforcé mes compétences en collaboration avec
+        d'autres développeurs, notamment dans la communication, la résolution de
+        problèmes et le partage de connaissances.
+      </p>
+      <p>
+        Green Code : Intégration des pratiques du Green Code dans un projet, et
+        les bénéfices que cela peut apporter.
+      </p>
     </p>
   );
 }
