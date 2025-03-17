0fad8d80acca6eca4822d0dd285e4ee3dbcfc816
Fabienholert <fabienholert@gmail.com>
2025-03-17 18:50:39 +0100

    Mise à jour du composant Projet1 : réorganisation du contenu en paragraphes pour améliorer la lisibilité et la structure.

--- portfolio/src/pages/projet1/projet1.jsx
+++ portfolio/src/pages/projet1/projet1.jsx
@@ -5,21 +5,32 @@ export default function Projet1() {
       <p>
         Développement de l'application React : Création de l'ensemble de
         l'application React à partir de zéro, en respectant les maquettes Figma
-        fournies par le designer. Implémentation des composants React :
-        Conception et développement de tous les composants React nécessaires, en
-        suivant une approche modulaire et réutilisable. Gestion des routes avec
-        React Router : Mise en place d'un système de routage robuste pour
-        permettre une navigation fluide et intuitive entre les différentes pages
-        du site. Intégration des données : Utilisation d'un fichier JSON
-        contenant les données des annonces de logements pour alimenter
-        l'application, en attendant la mise en place du backend. Respect des
-        contraintes techniques et des coding guidelines : Adhésion aux coding
-        guidelines de Kasa et utilisation de Vite comme outil de build.
+        fournies par le designer.{" "}
+      </p>
+      <p>
+        Implémentation des composants React : Conception et développement de
+        tous les composants React nécessaires, en suivant une approche modulaire
+        et réutilisable.
+      </p>
+      <p>
+        {" "}
+        Gestion des routes avec React Router : Mise en place d'un système de
+        routage robuste pour permettre une navigation fluide et intuitive entre
+        les différentes pages du site.{" "}
+      </p>
+      <p>
+        Intégration des données : Utilisation d'un fichier JSON contenant les
+        données des annonces de logements pour alimenter l'application, en
+        attendant la mise en place du backend. Respect des contraintes
+        techniques et des coding guidelines : Adhésion aux coding guidelines de
+        Kasa et utilisation de Vite comme outil de build.
+      </p>
+      <p>
         Implémentation des fonctionnalités spécifiques : Défilement infini de la
         galerie d'images avec gestion des cas limites (une seule image,
         première/dernière image). Fonctionnalité de Collapse (accordéon) avec
         ouverture et fermeture des sections au clic. Adaptation de l'interface
-        pour différents écrans (responsive design).{" "}
+        pour différents écrans (responsive design).
       </p>
       <h3> Problématiques rencontrées et solutions :</h3>
       <p>
@@ -27,33 +38,53 @@ export default function Projet1() {
         trouver une solution pour simuler les données et permettre le
         développement du front-end. J'ai utilisé le fichier JSON fourni et des
         variables d'état React pour gérer les données de manière dynamique.
+      </p>
+      <p>
         Respect des délais : La refonte complète du site était un projet
         ambitieux avec des délais serrés. J'ai dû m'organiser efficacement,
         prioriser les tâches et communiquer régulièrement avec l'équipe pour
-        m'assurer de respecter les échéances. Complexité des fonctionnalités :
-        Certaines fonctionnalités, comme le défilement infini de la galerie
-        d'images, présentaient des défis techniques. J'ai dû faire des
-        recherches approfondies, expérimenter différentes approches et
-        collaborer avec d'autres développeurs pour trouver les meilleures
-        solutions. Comprendre les besoins de l'entreprise : Au delà des
-        contraintes techniques, il a fallu saisir les enjeux de Kasa, comprendre
-        les utilisateurs et anticiper les éventuels problèmes.{" "}
+        m'assurer de respecter les échéances.{" "}
+      </p>
+      <p>
+        Complexité des fonctionnalités : Certaines fonctionnalités, comme le
+        défilement infini de la galerie d'images, présentaient des défis
+        techniques. J'ai dû faire des recherches approfondies, expérimenter
+        différentes approches et collaborer avec d'autres développeurs pour
+        trouver les meilleures solutions.{" "}
+      </p>
+      <p>
+        Comprendre les besoins de l'entreprise : Au delà des contraintes
+        techniques, il a fallu saisir les enjeux de Kasa, comprendre les
+        utilisateurs et anticiper les éventuels problèmes.
       </p>
       <h3>Compétences développées :</h3>
       <p>
         Maîtrise de React : Renforcement de mes compétences en React, notamment
         dans la création de composants réutilisables, la gestion de l'état et
-        l'utilisation de React Router. Gestion de projet front-end :
-        Amélioration de mes compétences en gestion de projet front-end,
-        notamment dans la planification, l'organisation et le suivi des tâches.
+        l'utilisation de React Router.{" "}
+      </p>
+      <p>
+        Gestion de projet front-end : Amélioration de mes compétences en gestion
+        de projet front-end, notamment dans la planification, l'organisation et
+        le suivi des tâches.
+      </p>
+      <p>
         Collaboration : Développement de mes compétences en collaboration avec
-        d'autres développeurs, designers et chefs de projet. Résolution de
-        problèmes : Amélioration de mes compétences en résolution de problèmes
-        techniques, notamment dans la recherche de solutions, l'expérimentation
-        et la collaboration. Autonomie : Renforcement de mon autonomie dans la
-        gestion de projet en freelance, notamment dans la prise de décision, la
-        communication et la gestion du temps. Travail en équipe : L'expérience
-        en freelance m'a permis de mieux cerner les rouages d'une entreprise.
+        d'autres développeurs, designers et chefs de projet.{" "}
+      </p>
+      <p>
+        Résolution de problèmes : Amélioration de mes compétences en résolution
+        de problèmes techniques, notamment dans la recherche de solutions,
+        l'expérimentation et la collaboration.
+      </p>
+      <p>
+        Autonomie : Renforcement de mon autonomie dans la gestion de projet en
+        freelance, notamment dans la prise de décision, la communication et la
+        gestion du temps.
+      </p>
+      <p>
+        Travail en équipe : L'expérience en freelance m'a permis de mieux cerner
+        les rouages d'une entreprise.
       </p>
     </p>
   );
