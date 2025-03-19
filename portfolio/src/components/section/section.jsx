4cd62b50fd2bb9b20af314c21459f59afe0988d0
Fabienholert <fabienholert@gmail.com>
2025-03-19 13:06:28 +0100

    Remplacement de la liste par un carrousel Splide pour la section des compétences, améliorant ainsi la présentation et l'interaction.

--- portfolio/src/components/section/section.jsx
+++ portfolio/src/components/section/section.jsx
@@ -2,40 +2,46 @@ import "./section.scss";
 
 export default function Section() {
   return (
-    <ul className="Section__Container">
-      <li className="Section__frontEnd">
-        Front-end
-        <p>
-          <strong>Langages :</strong> Javascript
-        </p>
-        <p>
-          <strong>Frameworks et Bibliothèques:</strong> React
-        </p>
-        <p>
-          <strong>Intégration Web:</strong> HTML , CSS , SASS
-        </p>
-      </li>
-      <li className="Section__Backend">
-        Back-end
-        <p>
-          <strong>Langages et Frameworks:</strong> Nodejs, Express.js
-        </p>
-        <p>
-          <strong>Base de Données:</strong> MongoDB
-        </p>
-        <p>
-          <strong>API:</strong> Création et consommation d'API REST
-        </p>
-      </li>
-      <li className="Section__Outils">
-        Outils
-        <p>
-          <strong>Gestion de version:</strong> Git, Github
-        </p>
-        <p>
-          <strong>Environnements:</strong> Visual Studio Code , Github
-        </p>
-      </li>
-    </ul>
+    <section class="splide" aria-labelledby="carousel-heading">
+      <h2 id="carousel-heading">Splide Basic HTML Example</h2>
+
+      <div class="splide__track">
+        <ul class="splide__list">
+          <li class="splide__slide">
+            <h3>Front-end</h3>
+            <p>
+              <strong>Langages :</strong> Javascript
+            </p>
+            <p>
+              <strong>Frameworks et Bibliothèques:</strong> React
+            </p>
+            <p>
+              <strong>Intégration Web:</strong> HTML , CSS , SASS
+            </p>
+          </li>
+          <li class="splide__slide">
+            <h3>Back-end</h3>
+            <p>
+              <strong>Langages et Frameworks:</strong> Nodejs, Express.js
+            </p>
+            <p>
+              <strong>Base de Données:</strong> MongoDB
+            </p>
+            <p>
+              <strong>API:</strong> Création et consommation d'API REST
+            </p>
+          </li>
+          <li class="splide__slide">
+            <h3>Outils</h3>
+            <p>
+              <strong>Gestion de version:</strong> Git, Github
+            </p>
+            <p>
+              <strong>Environnements:</strong> Visual Studio Code , Github
+            </p>
+          </li>
+        </ul>
+      </div>
+    </section>
   );
 }
