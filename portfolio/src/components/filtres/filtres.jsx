c87f2beb09c121744192cec3616f93cfe2f45891
Fabienholert <fabienholert@gmail.com>
2025-03-19 18:41:19 +0100

    Ajout des boutons de filtrage dans le composant Filtres

--- portfolio/src/components/filtres/filtres.jsx
+++ portfolio/src/components/filtres/filtres.jsx
@@ -1,5 +1,12 @@
 import "./filtres.scss";
 
 export default function Filtres() {
-  return <div></div>;
+  return (
+    <div className="filter">
+      <button className="filter__button">Tous</button>
+      <button className="filter__button">Front-end</button>
+      <button className="filter__button">Back-end</button>
+      <button className="filter__button">Gestion de projet</button>
+    </div>
+  );
 }
