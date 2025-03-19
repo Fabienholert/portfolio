6b1f9f5e3f1b3be6494ec6a06dedb376d6ab50f8
Fabienholert <fabienholert@gmail.com>
2025-03-19 14:24:16 +0100

    Ajout d'un conteneur pour la section des compétences avec amélioration du style CSS pour une meilleure présentation.

--- portfolio/src/components/section/section.jsx
+++ portfolio/src/components/section/section.jsx
@@ -6,41 +6,43 @@ export default function Section() {
   return (
     <div className="section">
       <h2>Compétences</h2>
-      <Splide aria-label="My Favorite Images">
-        <SplideSlide>
-          <h3>Front-end</h3>
-          <p>
-            <strong>Langages :</strong> Javascript
-          </p>
-          <p>
-            <strong>Frameworks et Bibliothèques:</strong> React
-          </p>
-          <p>
-            <strong>Intégration Web:</strong> HTML , CSS , SASS
-          </p>
-        </SplideSlide>
-        <SplideSlide>
-          <h3>Back-end</h3>
-          <p>
-            <strong>Langages et Frameworks:</strong> Nodejs, Express.js
-          </p>
-          <p>
-            <strong>Base de Données:</strong> MongoDB
-          </p>
-          <p>
-            <strong>API:</strong> Création et consommation d'API REST
-          </p>
-        </SplideSlide>
-        <SplideSlide>
-          <h3>Outils</h3>
-          <p>
-            <strong>Gestion de version:</strong> Git, Github
-          </p>
-          <p>
-            <strong>Environnements:</strong> Visual Studio Code , Github
-          </p>
-        </SplideSlide>
-      </Splide>
+      <div className="section__text">
+        <Splide aria-label="My Favorite Images">
+          <SplideSlide>
+            <h3>Front-end</h3>
+            <p>
+              <strong>Langages :</strong> Javascript
+            </p>
+            <p>
+              <strong>Frameworks et Bibliothèques:</strong> React
+            </p>
+            <p>
+              <strong>Intégration Web:</strong> HTML , CSS , SASS
+            </p>
+          </SplideSlide>
+          <SplideSlide>
+            <h3>Back-end</h3>
+            <p>
+              <strong>Langages et Frameworks:</strong> Nodejs, Express.js
+            </p>
+            <p>
+              <strong>Base de Données:</strong> MongoDB
+            </p>
+            <p>
+              <strong>API:</strong> Création et consommation d'API REST
+            </p>
+          </SplideSlide>
+          <SplideSlide>
+            <h3>Outils</h3>
+            <p>
+              <strong>Gestion de version:</strong> Git, Github
+            </p>
+            <p>
+              <strong>Environnements:</strong> Visual Studio Code , Github
+            </p>
+          </SplideSlide>
+        </Splide>
+      </div>
     </div>
   );
 }
