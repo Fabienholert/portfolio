a64963c3e88ef1215726f9309de209a33bcfef11
Fabienholert <fabienholert@gmail.com>
2025-03-20 19:31:08 +0100

    Ajout d'un formulaire de contact et amélioration des styles dans les composants Footer et Home

--- src/components/formulaire/formulaire.jsx
+++ src/components/formulaire/formulaire.jsx
@@ -4,6 +4,13 @@ export default function Formulaire() {
   return (
     <div id="contact" className="formulaire">
       <h2>Contactez-moi</h2>
+      <form>
+        <input type="text" placeholder="Nom" />
+        <input type="email" placeholder="Email" />
+        <input type="text" placeholder="Objet" />
+        <textarea placeholder="Message"></textarea>
+        <button type="submit">Envoyer</button>
+      </form>
     </div>
   );
 }
