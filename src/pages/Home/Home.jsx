ebbfd51012d88aad17018edb182c54fbb8047492
Fabienholert <fabienholert@gmail.com>
2025-03-20 17:05:09 +0100

    Ajout d'une bannière au composant Home avec réorganisation des éléments et intégration d'une image de développeur.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Dev from "../../assets/images/dev.jpg";
 import Photo from "../../assets/images/moi.webp";
 import Formulaire from "../../components/formulaire/formulaire";
 import Projets from "../../components/projets/projets";
@@ -8,22 +9,26 @@ import "./Home.scss";
 export default function Home() {
   return (
     <div>
-      <div className="home__Container">
-        <h1 className="home__nom">Bienvenue, je suis Fabien Holert</h1>
-        <h2 className="home__titre">
-          <strong> Développeur Web</strong>
-        </h2>
-        <a href="#mes_projets" className="bouton__mesprojets">
-          Mes projets
-        </a>
-
-        <img src={Photo} alt="moi" className="home__photo" />
-        <p id="presentation" className="home__description">
-          Passionné par le développement web, je suis actuellement en formation
-          chez OpenClassrooms pour devenir développeur web. J'ai acquis des
-          compétences en HTML, CSS, Javascript, React, Nodejs, Express.js et
-          MongoDB.
-        </p>
+      <div className="home__banner">
+        <div className="home__Container">
+          <img src={Dev} alt="image developpeur" className="home__dev" />
+          <h1 className="home__nom">Bienvenue, je suis Fabien Holert</h1>
+          <h2 className="home__titre">
+            <strong> Développeur Web</strong>
+          </h2>
+          <a href="#mes_projets" className="bouton__mesprojets">
+            Mes projets
+          </a>
+        </div>
+        <div className="home__presentation">
+          <img src={Photo} alt="photo de moi" className="home__photo" />
+          <p id="presentation" className="home__description">
+            Passionné par le développement web, je suis actuellement en
+            formation chez OpenClassrooms pour devenir développeur web. J'ai
+            acquis des compétences en HTML, CSS, Javascript, React, Nodejs,
+            Express.js et MongoDB.
+          </p>
+        </div>
       </div>
       <Section />
       <Projets />
