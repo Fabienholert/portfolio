e635d42f7686597eb6425b48ec41130fc36112e9
Fabienholert <fabienholert@gmail.com>
2025-03-20 14:48:26 +0100

    Ajout d'un identifiant au conteneur du composant Formulaire pour faciliter le ciblage dans la navigation.

--- portfolio/src/components/formulaire/formulaire.jsx
+++ portfolio/src/components/formulaire/formulaire.jsx
@@ -2,7 +2,7 @@ import "./formulaire.scss";
 
 export default function Formulaire() {
   return (
-    <div className="formulaire">
+    <div id="contact" className="formulaire">
       <h2>Contactez-moi</h2>
     </div>
   );
