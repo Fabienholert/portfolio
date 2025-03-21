0ea37e277e7516532a5a63d65230095fc3b13bcc
Fabienholert <fabienholert@gmail.com>
2025-03-21 18:51:26 +0100

    Suppression des routes pour les projets 1 et 2 dans le composant App

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Layout from "./layout.jsx";
 import Home from "./pages/Home/Home.jsx";
-import Projet1 from "./pages/projet1/projet1.jsx";
-import Projet2 from "./pages/projet2/projet2.jsx";
 
 export default function App() {
   return (
@@ -12,8 +10,6 @@ export default function App() {
         <Routes>
           <Route element={<Layout />}>
             <Route path="/" element={<Home />} />
-            <Route path="/projet1" element={<Projet1 />} />
-            <Route path="/projet2" element={<Projet2 />} />
           </Route>
         </Routes>
       </BrowserRouter>
