a8cfc483030c6c8b88b452eb95e5c1b273bde59d
Fabienholert <fabienholert@gmail.com>
2025-03-21 11:59:11 +0100

    Ajout d'un composant MyComponent pour afficher les projets à partir d'un fichier JSON

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,47 +1,38 @@
+import React, { useEffect, useState } from "react";
 import "./card.scss";
 
-export default function Card() {
+function MyComponent() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch("projects.json"); // Chemin vers votre fichier JSON
+      const data = await response.json();
+      setProjects(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <div className="card">
-        <div className="card_details">
-          <div className="details__title" />
-          <div className="details__description" />
-          <div className="details__tags" />
-          <div className="details__info" />
-        </div>
-        <h3 className="details__title"></h3>
-        <img></img>
-      </div>
-      <div className="modal modal--hidden">
-        <div className="modal__background" />
-        <div className="modal__content">
-          <div className="modal__close">button</div>
-          <div className="modal__children">
-            <h3 className="card-modal__title"></h3>
-            <div className="card-modal__content">
-              <div>
-                <div className="card-modal__context">
-                  <h4></h4>
-                  <p></p>
-                </div>
-                <div className="card-modal__challenge">
-                  <h4></h4>
-                  <p></p>
-                </div>
-                <div className="card-modal__right-colon">
-                  <div className="card-modal__links">
-                    <a href=""></a>
-                  </div>
-                  <div className="card-modal__technologies">
-                    <div className="tags"></div>
-                  </div>
-                </div>
-              </div>
+      {projects.map((project) => (
+        <div className="card" key={project.id}>
+          <div className="card_details">
+            <div className="details__title">{project.card.details.title}</div>
+            <div className="details__description">
+              {project.card.details.description}
             </div>
+            <div className="details__tags">
+              {project.card.details.tags.join(", ")}
+            </div>
+            <div className="details__info">{project.card.details.info}</div>
           </div>
+          <h3 className="details__title">{project.card.title}</h3>
+          <img src={project.card.image} alt={project.card.title} />
         </div>
-      </div>
+      ))}
     </div>
   );
 }
+
+export default MyComponent;
