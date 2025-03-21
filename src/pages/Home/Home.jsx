8686e875fd43f88f2f078139eb8dcb6bf05e13c9
Fabienholert <fabienholert@gmail.com>
2025-03-21 23:08:02 +0100

    Mise à jour de la page d'accueil : amélioration de la présentation personnelle, ajout d'un titre de section et intégration du composant Compétence.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import Dev from "../../assets/images/dev.jpg";
 import Photo from "../../assets/images/moi.webp";
+import Section from "../../components/competence/Competence";
 import Formulaire from "../../components/formulaire/Formulaire";
 import Projets from "../../components/projets/Projets";
-import Section from "../../components/competence/Competence";
 import "./Home.scss";
 
 export default function Home() {
@@ -12,23 +12,25 @@ export default function Home() {
       <div className="home__banner">
         <div className="home__Container">
           <img src={Dev} alt="image developpeur" className="home__dev" />
-          <h1 className="home__nom">Bienvenue, je suis Fabien Holert</h1>
-          <h2 className="home__titre">
-            <strong> Développeur Web</strong>
-          </h2>
+          <h1 className="home__nom">
+            Bienvenue, je suis Fabien Holert, Développeur Web
+          </h1>
           <a href="#mes_projets" className="home__button">
             Mes projets
           </a>
         </div>
       </div>
       <div id="presentation" className="home__presentation">
-        <img src={Photo} alt="photo de moi" className="home__photo" />
+        <h2 className="home__presentation-titre">Présentation</h2>
         <p className="home__description">
-          Passionné par le développement web, je suis actuellement en formation
-          chez OpenClassrooms pour devenir développeur web. J'ai acquis des
-          compétences en HTML, CSS, Javascript, React, Nodejs, Express.js et
-          MongoDB.
+          Je m'appelle Fabien Holert, j'ai 40 ans et je suis passionné par le
+          développement web. J'ai décidé de me reconvertir dans ce domaine et
+          j'ai commencé une formation chez OpenClassrooms pour devenir
+          développeur web. J'ai déjà acquis des compétences en HTML, CSS,
+          Javascript, React et Node.js pour devenir un développeur fullstack.
         </p>
+        <img src={Photo} alt="photo de moi" className="home__photo" />
+        <p className="home__description"></p>
       </div>
       <Section />
       <Projets />
