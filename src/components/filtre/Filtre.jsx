17fda1d9e70e2977976270ef60fbc7c837afde1b
Fabienholert <fabienholert@gmail.com>
2025-03-22 11:39:00 +0100

    Refactorisation du composant Filtre : suppression de l'état et des effets inutilisés, simplification du rendu.

--- src/components/filtre/Filtre.jsx
+++ src/components/filtre/Filtre.jsx
@@ -1,16 +1,8 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./filtre.scss";
 
 function Filtre() {
-  const [datas, setDatas] = useState([]);
-  useEffect(() => {
-   
-  }, []);
-
-  console.log(datas);
- <div className="filtres">{datas.map()=> {
-  return (<div></div>)
-  }}</div>;
+  return <div>Filtre</div>;
 }
 
 export default Filtre;
